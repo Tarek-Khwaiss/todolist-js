12af4026541b538725f852a634a1f8010d3ccde2
Tarek-khwaiss <tarekonguitar@hotmail.com>
2022-12-10 19:43:14 +0200

    please

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const formAdd = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todos');
 const deleteTodo = document.querySelector('.delete');
+const formSearch = document.querySelector('.search');
 
 // 2 methods
 const generateTodo = (todo) => {
@@ -22,12 +23,11 @@ formAdd.addEventListener('submit', (e) => {
     // attach whats inside the form to the new li
     const newtodo = formAdd.newtodo.value.trim();
     if (newtodo.length) {
-        li.textContent = newtodo;
-        // generateTodo(newtodo);
-        // formadd.reset();
-        todoList.append(li);
-        li.classList = "list-group-item d-flex justify-content-between align-items-center";
-        li.innerHTML += '<i class="fa-solid fa-trash delete"></i>';
+        // li.textContent = newtodo;
+        // todoList.append(li);
+        // li.classList = "list-group-item d-flex justify-content-between align-items-center";
+        // li.innerHTML += '<i class="fa-solid fa-trash delete"></i>';
+        generateTodo(newtodo);
         formAdd.reset();
     }
 });
@@ -38,4 +38,31 @@ todoList.addEventListener('click', (e) => {
     if (element.classList.contains("delete")) {
         element.parentElement.remove();
     }
+});
+
+// filtering todos, search mechanisim
+formSearch.addEventListener('keyup', () => {
+    // get the current text input
+    const currentString = formSearch.search.value;
+
+    // get the list of li's to loop on them
+    let li = todoList.querySelectorAll('li');
+
+    let length = todoList.children.length;
+    //console.log(li);
+
+    // loop on the list of li's
+    for (let i = 0; i < length; i++) {
+        let element = li[i].getElementsByTagName('span')[0];
+        //console.log(li[i]);
+        if (element.textContent.indexOf(currentString) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+    // if (todoList.childNodes.forEach((element) => {
+    //     if (!element.contains(currentString))
+    //         element.classList += 'd-none';
+    // }));
 });
